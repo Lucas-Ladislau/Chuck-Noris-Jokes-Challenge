123b86c74ff3b11c3358bbb3dc62a2bc472ca4f2
lucas.aguiar <laanderson183@gmail.com>
2023-08-18 08:17:06 -0400

    intent to share the joke

--- app/src/main/java/com/mentoria/chuck_norris_jokes/MainActivity.kt
+++ app/src/main/java/com/mentoria/chuck_norris_jokes/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.mentoria.chuck_norris_jokes
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
 import com.mentoria.chuck_norris_jokes.model.Joke
 import com.mentoria.chuck_norris_jokes.network.APIClient
 import com.mentoria.chuck_norris_jokes.network.APIInterface
@@ -15,6 +19,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    //perguntar o pq de não funcionar dessa forma
+    //var jokeTextView: TextView = findViewById(R.id.joke)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,9 +28,19 @@ class MainActivity : AppCompatActivity() {
 
         getJoke()
         val refreshButton: Button = findViewById(R.id.newJoke)
+        val sharebutton: ImageButton = findViewById(R.id.shareButton)
+        val jokeTextView: TextView = findViewById(R.id.joke)
 
         refreshButton.setOnClickListener(){
             getJoke()
+        }
+        sharebutton.setOnClickListener(){
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/plain"
+            jokeTextView.text
+            intent.putExtra("Share", jokeTextView.text)
+            val chooser = Intent.createChooser(intent, "Share this Joke, Cowboy")
+            startActivity(chooser)
         }
 
     }
@@ -32,16 +48,15 @@ class MainActivity : AppCompatActivity() {
     fun getJoke(){
         val retrofitAPIClient = APIClient.getRetrofitInstance("https://api.chucknorris.io/")
         val endpoint = retrofitAPIClient.create(APIInterface::class.java).getJoke()
-
+        val jokeTextView: TextView = findViewById(R.id.joke)
 
         endpoint.enqueue(object : Callback<Joke> {
             override fun onResponse(call: Call<Joke>, response: Response<Joke>) {
                 if (response.isSuccessful){
                     val joke: Joke? = response.body()
                     if (joke != null){
-                        val value = joke.value
-                        val jokeTextView: TextView = findViewById(R.id.joke) // ou use o Data Binding se estiver configurado
-                        jokeTextView.text = value
+
+                        jokeTextView.text = joke.value
                     }
                 }else{
                     println("Resposta não bem sucedida: ${response.code()}")
